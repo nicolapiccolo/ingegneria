30ab382ba90f378e0c3935a6fde68c6f35b21901
Nicola Piccolo <nikola9piccolo9@gmail.com>
2020-01-17 12:07:14 +0100

    aggiornata path cartella immagini

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-PATH = '/Users/nicopiccolo/Desktop/monumenti'
+PATH = 'monumenti'
 
 train_dir = os.path.join(PATH, 'train')
 validation_dir = os.path.join(PATH, 'test')
